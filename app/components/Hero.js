7a959f9d1e75ef6174a721544e540757c7ecedf2
Soumyadeep-4704 <soumyadeeppaul092023@gmail.com>
2025-11-15 14:07:37 +0530

    Added a hero section

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -3,24 +3,26 @@
 import { useSession } from "next-auth/react";
 
 export default function Hero() {
-    const {data: session, status} = useSession();
+  const { data: session, status } = useSession();
 
-    // If the user is Logged In
-    if(status === "authenticated"){
-        let userName = session.user.name;
+  // If the user is Logged In
+  if (status === "authenticated") {
+    let userName = session.user.name;
 
-        return (
-        <div>
-            <h1>Welcome, {userName}! </h1>
-        </div>
-    );
-    }
-
-    // If the user is not Logged In
     return (
-        <div>
-            <h1>Welcome, Guest!</h1>
-            <p>Please login to start</p>
-        </div>
+      <div className="flex min-h-full flex-col items-center justify-center p-24">
+        <h1 className="text-5xl font-bold font-mono bg-gradient-to-r from-[#0ed7a8] to-[#417505] bg-clip-text text-transparent">Welcome, {userName}! </h1>
+      </div>
     );
+  }
+
+  // If the user is not Logged In
+  return (
+    <div className="flex min-h-full flex-col items-center justify-center p-24">
+      <h1 className="text-5xl font-bold font-mono bg-gradient-to-r from-[#0ed7a8] to-[#417505] bg-clip-text text-transparent">Welcome, Guest!</h1>
+      <p className="text-sm font-bold font-mono">
+        Log in to get started. Use GitHub for the best experience, or sign in with the Google account linked to your GitHub.
+      </p>
+    </div>
+  );
 }
