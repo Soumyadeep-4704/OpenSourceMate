e10f8141c61bf566064886b98422e01d609cfd12
SOUMYADEEP PAUL <soumyadeeppaul.info@gmail.com>
2026-01-03 02:43:24 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -6,27 +6,25 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      allowDangerousEmailAccountLinking: true,
-      // Request permission to read repositories and issues
-      authorization: { params: { scope: "read:user user:email repo" } },
+      authorization: {
+        params: {
+          scope: "read:user user:email public_repo",
+        },
+      },
     }),
   ],
-  // Add callbacks to pass the access token to the client
   callbacks: {
     async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token;
       }
       return token;
     },
     async session({ session, token }) {
-      // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken;
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
