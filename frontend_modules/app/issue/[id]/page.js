aea4ea29079eaaed907183c8f0f49fd31c420482
Soumyadeep-4704 <soumyadeeppaul.info@gmail.com>
2026-01-04 21:51:15 +0530

    imports updated

--- frontend_modules/app/issue/[id]/page.js
+++ frontend_modules/app/issue/[id]/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "../../api/auth/[...nextauth]/route";
 
 export default async function IssueDetail({params}) {
   // Get ID from URL
